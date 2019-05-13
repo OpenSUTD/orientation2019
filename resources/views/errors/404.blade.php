9a201412ecc9271378923f4b13f402c05ac847fc
chesnutcase <chester8991@gmail.com>
2019-05-13 16:44:07 +0800

    add hidden puzzle

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,144 @@
-@extends('errors::illustrated-layout')
-
-@section('code', '404')
-@section('title', __('Page Not Found'))
-
-@section('image')
-    <div style="background-image: url({{ asset('/svg/404.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+<html>
+  <head>
+    <style>
+    .message_box{
+      border: 2px solid blue;
+      max-width: 100vw;
+      height: auto;
+      word-break: break-word;
+      font-family: monospace;
+    }
+    .actual_message{
+      display: none;
+    }
+    .expected_password{
+      display: none;
+    }
+    .message_group{
+      width:100%;
+    }
+    .guess_password{
+      width: 50%;
+      height: 5vh;
+      font-size:3em;
+      border: 1px solid red;
+    }
+    body{
+      overflow-x: hidden;
+    }
+    </style>
+    <script>
+    window.addEventListener("DOMContentLoaded", function(){
+      function makeid(length) {
+         var result           = '';
+         var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+         var charactersLength = characters.length;
+         for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+         }
+         return result;
+      }
+      var inputs = Array.from(document.querySelectorAll('.guess_password'));
+      inputs.forEach(function(input, pos){
+        let expected_password = document.querySelectorAll('.expected_password')[pos].textContent.trim();
+        let actual_message = document.querySelectorAll('.actual_message')[pos].innerHTML;
+        let message_box = document.querySelectorAll('.message_box')[pos];
+        input.addEventListener("keyup", function(e){
+          if(input.value == expected_password){
+            message_box.innerHTML = actual_message;
+          }else{
+            message_box.innerHTML = makeid(256);
+          }
+        });
+      });
+    });
+    </script>
+  </head>
+  <body>
+    <h1>404</h1>
+    <h2>Page Not Found</h2>
+    <h4>However, we found <a>these</a> instead......</h4>
+    
+    <div class="message_group">
+      <div></div>
+      <div>
+        L1: ///dismissing.measuring.seducing<br>
+        L2: L1 + (212.4km, 75.13o)<br>
+        L3: ///allegiance.glittered.consequence<br>
+        L4: L3 + (536.3km, 90.06o)<br>
+        L5: ///smart.unfailing.unbeatable<br>
+        L6: L5 + (36.61km, 241.9o)<br>
+      </div>
+      <div class="actual_message">
+<b>Enter <code>stc2200etcpanvcp</code> to earn your group an extra 300 points</b><br>
+Multinational climate refugee colony opens in Burlington
+<br><br>
+<br><br>CORSHAM, UK - May 15, 2244 - Celebrated artist and environmental activist Diego Manrique fought back tears today in a speech delivered on the steps of the newly opened Burlington Colony, hundreds of feet below the sprawling urban jungle of Corsham, Wiltshire.
+<br><br>“I was only seventeen in 2223, when the waves rose up and swallowed the Canary Islands,” Manrique said. “The next decade of my life, I spent fighting for survival in the Wroughton Emergency Relocation Zone, our tents at the mercy of dust storms, heat waves and torrential rain.” Flanked by activist group leaders, fellow refugees and survivors, Manrique unveiled a series of three wall murals in the main tunnel-way of the Colony entrance, dedicated to climate casualties around the world. “As we step into the future of mankind’s survival, may they never be forgotten.”
+<br><br>Third-Chance Party UK Prime Minister Lydia O’Connor, who first proposed the construction of the Colony, was among the first to tour the new complex. “ [Burlington] was once an emergency government war HQ. Today, it seems we are at war with Mother Earth herself. We must all do what we can to prepare ourselves, and I promise Burlington will be the first of many.”
+<br><br>In contrast, main opposition candidate Oliver Byrne of the Revoke-1338 party was fiercely critical of the Colony. “Instead of pouring billions into glorified underground slums, we should have begun establishing lunar settlements a long time ago. There is nothing - no hope - left for us here.” Byrne’s remarks echo the sentiments of major opposition groups worldwide, as the Cheyenne, Sonnenberg, and Dixiacheng Colonies are just beginning operations. “It’s ridiculous. Not a single off-planet human… [DATA CORRUPTED]
+<br><br>
+<br><br>1.2
+<br><br>Investors rejoice as BNL shares rally after third colony established
+<br><br>
+<br><br>CAPE COLONY, FL - January 4, 2256 - After a surprise announcement by Bayer-Nowell Land Infrastructure [BNL] on Thursday, BNL investors began the year on a high with a stunning 7.3% hike in 24 hours.
+<br><br>Despite the good news, analysts urge restraint, noting that the profitability of above-ground colonies tends to be unstable in the near-term. Bayer-Nowell will have to contend with the initial difficulties of managing over 70,000 people in the Cape Colony, their third and largest one yet, though co-founder and CEO Martin Nowell maintains that there will be “no issue, given our extensive track record in open-air climate-defense.
+<br><br>BNL’s announcement comes closely on the heels of PANV Pharmaceutics’ [PANV] acquisition of Crater Lake, Raven Rock, and Brown Ridge, bringing their tally to 34 colonies. And after the messy dissolution of GZK Inc last month, PANV looks set to annex another six colonies in the former United Republic of Korea. Helmed by its four phenomenal directors, PANV has a clear lead, but in this post-government economy it remains to be seen if there isn’t enough room for multiple… [DATA CORRUPTED]
+      </div>
+      <div class="expected_password">xenium</div>
+      <div class="message_box">
+        @php
+         echo str_random(256);
+        @endphp
+      </div>
+      <br/>
+      <span style="font-size:3em">Enter decryption key:</span><input type="password" placeholder="<keygoeshere>" class="guess_password">
     </div>
-@endsection
-
-@section('message', __('Sorry, the page you are looking for could not be found.'))
+    
+    <div class="message_group">
+      <div>5 fyra 5 fem 14 6 tre 14 3 sex 2 2 fem</div>
+      <div class="actual_message">
+<b>Enter <code>tff4semabaacaftl</code> to earn your group extra 200 points</b><br>
+2.1
+<br><br>Holo-ad, dated around 2270
+<br><br>Have you heard? There’s a Cydroid Pod Cafe coming to YOUR neighborhood!
+<br><br>This is a special offer exclusively for [PRINTVALUE:$RECIPIENT]. Did you know that a Cydroid Pod Cafe, PANV Pharmaceutic’s premier mixed-reality recreational facility, is opening a location near you? Would you like to ski the ashen slopes of the Andes, take a vacation to the bottom of the ocean, or explore the deserts of Brazil? All that and more is possible when you connect to one of PANV’s flagship Cydroids!
+<br><br>
+<br><br>Make a reservation at our new [PRINTVALUE:$RECIPIENTLOCATION] location today, and save 15% off your tab when you purchase one of our new Mk II Cydroids! 
+<br><br>
+<br><br>2.2
+<br><br>Editorial: Cydroids - unhealthy fad, or future?
+<br><br>April 14, 2271 - Call me old-fashioned, but there is something about a magical device that slows your aging (or so they claim) and projects your consciousness into a hollow, humanoid shell that gives me the heebie-jeebies. And now “Cydroid Pod Cafes”!? Next, you’re going to tell me Stim and Inebri8 are doing society a favor!
+<br><br>While this editor can plainly see its potential for use in cases of the terminally ill and immobile, PANV Megacorp’s latest foray into questionable technological advancements seems to completely disregard… [DATA CORRUPTED]
+<br><br>
+<br><br>2.3
+<br><br>Directors’ Meeting Minutes, July 24, 2273
+<br><br>Trent Paver: ...the good news is, our initial analysis has brought up estimates that are off the charts. The bad news is that it’s stuck in big, dumb rocks, but Abby’s got it, right?
+<br><br>Joanne Auborn: I’m not sure our Mk II’s can last for long in those conditions, let alone  get up there. And the Mk III’s are designed for extreme Earth environments.
+<br><br>Mika Nakano: If I may interject, Kennedy’s old, but serviceable, and it shouldn’t be difficult to work on Luna for manufacturing our Mk IIIs once we get there..
+<br><br>Robert Vetsch: I’ve been saying it all along, there’s heaps more potential in those rocks than we have left on terra firma. It’s bleeding-edge or nothing, and we can’t afford nothing.
+<br><br>Trent Paver: Joanne, Mika, how long do you think it’ll take for the Mk III to get off the ground? Manpower won’t be a problem. The Cydroids themselves, though...
+<br><br>Joanne Auborn: Nobody’s been up there in forever, so it’s anybody’s guess if the new plans work. At least, I certainly impressed myself with some of the new designs.
+<br><br>Mika Nakano: Manufacturing’s squeezing as much as we can out of what we have left. That’s not much, even accounting for the recycled Mk I’s. We’re really doing this?
+<br><br>Robert Vetsch: Damn right, we’re doing this. We’re going to space.
+<br><br>[END OF RECORDING]
+<br><br>
+<br><br>2.4
+<br><br>CALLING ALL ABLE-MINDED PERSONS!
+<br><br>(Archivist’s note: Holo-ad dated around 2274)
+<br><br>Calling all able-minded persons!
+<br><br>PANV Pharmaceutics is now hiring Full-Time Cydroid Operators from the following Colonies: [PRINTVALUE:$RECIPIENTLOCATION]. You will be housed in brand new, fully customized subterranean facilities with state-of-the-art security, and all your physical needs will be fully met within our newest Mk III Cydroid Pod System! (patent pending).
+<br><br>
+<br><br>Your job scope will include space exploration, mining, manufacturing, and base-founding. Sign up today at… [DATA CORRUPTED]
+<br><br></div>
+      <div class="expected_password">ruy lopez</div>
+      <div class="message_box">
+        @php
+         echo str_random(256);
+        @endphp
+      </div>
+      <br/>
+      <span style="font-size:3em">Enter decryption key:</span><input type="password" placeholder="<keygoeshere>" class="guess_password">
+    </div>
+  </body>
+</html>
